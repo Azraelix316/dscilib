09190090b5aa04a954473a0038864cd5925a0241
Azraelix316 <jaredj13310@gmail.com>
2025-07-07 16:31:18 +0800

    Add tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,52 @@
+#include "lib.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <vector>
+
+double linReg(double input, std::vector<double> coefficients) {
+  return coefficients[0] * input + coefficients[1];
+}
+
+int main() {
+  std::vector<std::vector<double>> data;
+  std::vector<double> coefficients{0, 0};
+  srand(time(0));
+  std::vector<double> initial_coefficients{static_cast<double>(rand()),
+                                           static_cast<double>(rand())};
+  coefficients = initial_coefficients;
+  data = dscilib::read_from_csv_double("tests.csv");
+  data = dscilib::rotate90CW(data);
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 10000; i++) {
+    /*dscilib::coord_descent(coefficients, data[0], data[1], linReg);*/
+  }
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::high_resolution_clock::now() - start);
+  coefficients = initial_coefficients;
+  std::cout << duration.count() << " ns" << std::endl;
+  start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 10000; i++) {
+    /*dscilib::new_coord_descent(coefficients, data[0], data[1], linReg);*/
+  }
+  duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::high_resolution_clock::now() - start);
+  std::cout << duration.count() << " ns" << std::endl;
+
+  coefficients = {0, 0};
+  data = dscilib::read_from_csv_double("tests.csv");
+  data = dscilib::rotate90CW(data);
+  for (int j = 0; j < data.size(); j++) {
+    for (int i = 0; i < data[j].size(); i++) {
+      std::cout << data[j][i] << " ";
+    }
+    std::cout << std::endl;
+  }
+  for (int i = 0; i < 10000; i++) {
+    /*dscilib::new_coord_descent(coefficients, data[0], data[1], linReg);*/
+  }
+  std::cout << coefficients[0] << "     " << coefficients[1] << "\n";
+}
