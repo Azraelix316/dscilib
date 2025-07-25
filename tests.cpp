25e152a19012adfb538123d7e5866d18ef861a9c
Azraelix316 <jaredj13310@gmail.com>
2025-07-25 18:04:57 +0800

    Add matrix mult function

--- tests.cpp
+++ tests.cpp
@@ -31,24 +31,14 @@ double quadratic(double input) {
   return input * input * input - 3 * input + 2 * input * input;
 }
 int main() {
-  std::vector<std::vector<double>> data =
-      dscilib::read_csv_double("tests.csv");
-  data = dscilib::transpose_matrix(data);
-  std::vector<double> coefficients{0, 0};
-  /*for (int i = 0; i < data.size(); i++) {*/
-  /*  inputs.push_back({data[i][0], data[i][1]});*/
-  /*}*/
-  /*std::vector<double> outputs;*/
-  /*for (int i = 0; i < data.size(); i++) {*/
-  /*  outputs.push_back(data[i][2]);*/
-  /*}*/
-  for (int i = 0; i < 100; i++) {
-    dscilib::coordinate_descent(coefficients, data[0], data[1], linReg);
+
+  std::vector<std::vector<double>> m1{{1, 1, 3}, {2, 3, 3}, {3, 3, 3}};
+  std::vector<std::vector<double>> m2{{3, 3, 2}, {4, 4, 2}};
+  std::vector<std::vector<double>> data = dscilib::matrix_mult(m1, m2);
+  for (std::vector<double> row : data) {
+    for (double ele : row) {
+      std::cout << ele << "  ";
+    }
+    std::cout << std::endl;
   }
-  double x=30.0;
-  for (int i=0;i<10;i++) {
-    dscilib::newton_root(x, quadratic);
-  }
-  std::cout << "Root found: " << x << std::endl;
-  std::cout << coefficients[0] << "  " << coefficients[1];
 }
