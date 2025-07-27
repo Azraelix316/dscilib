3d035a6c693ea2e45a43abd7400bdca72452ffcc
Azraelix316 <jaredj13310@gmail.com>
2025-07-27 11:16:44 +0800

    Add matrix_vec_mult function

--- tests.cpp
+++ tests.cpp
@@ -1,4 +1,5 @@
 #include "lib.hpp"
+#include <chrono>
 #include <iostream>
 #include <ostream>
 #include <vector>
@@ -34,8 +35,18 @@ int main() {
 
   std::vector<std::vector<double>> m1{
       {2.5, 2.4}, {0.5, 0.7}, {2.2, 2.9}, {1.9, 2.2}, {3.1, 3}};
-  std::vector<double> PC1 = dscilib::PCA(m1, 2)[0];
-  std::cout << PC1[0] << "  " << PC1[1] << std::endl;
-  std::vector<double> PC2 = dscilib::PCA(m1, 2)[1];
-  std::cout << PC2[0] << "  " << PC2[1] << std::endl;
+
+  auto start = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 100000; i++)
+    std::vector<double> PC1 = dscilib::PCA(m1, 2)[0];
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::high_resolution_clock::now() - start);
+  std::cout << duration.count() << "  ns" << std::endl;
+
+  auto start2 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 100000; i++)
+    std::vector<double> PC1fast = dscilib::NEW_PCA(m1, 2)[0];
+  auto duration2 = std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::high_resolution_clock::now() - start2);
+  std::cout << duration2.count() << "  ns" << std::endl;
 }
