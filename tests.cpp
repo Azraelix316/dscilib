7cce98a78d27a34adc14c76e707a64831f02e224
Azraelix316 <jaredj13310@gmail.com>
2025-10-22 22:16:48 +0800

    Flip input and coefficient order in a ssqr call

--- tests.cpp
+++ tests.cpp
@@ -3,19 +3,20 @@
 #include <iostream>
 #include <ostream>
 #include <vector>
-double function(std::vector<double> coefficients, std::vector<double> inputs) {
-  return coefficients[0] * inputs[0] + coefficients[1] * inputs[1] +
-         coefficients[2];
+double function(std::vector<double> coefficients, double inputs) {
+  return coefficients[0] * inputs + coefficients[1];
 }
 int main() {
-  std::vector<double> coefficients = {1.0, 1.0, 1.0};
-  std::vector<std::vector<double>> inputs =
-      dscilib::read_csv_double("tests.csv");
+  std::vector<double> coefficients = {1.0, 1.0};
+  std::vector<double> inputs =
+      dscilib::to_transposed(dscilib::read_csv_double("tests.csv"))[0];
   std::vector<double> outputs = {1, 2, 4, 6, 8, 11};
-  dscilib::detail::printArr(inputs);
-  dscilib::detail::printVec(outputs);
-  dscilib::coordinate_descent(coefficients, inputs, outputs, function, 0.5);
-  dscilib::detail::printVec(coefficients);
+  /*for (int i = 0; i < 1000; i++) {*/
+  /*  dscilib::coordinate_descent_iter(coefficients, inputs, outputs,
+   * function);*/
+  /*}*/
+  dscilib::coordinate_descent(coefficients, inputs, outputs, function, 0.49);
+  std::cout << coefficients[0] << "  " << coefficients[1];
   for (int i = 0; i < inputs.size(); i++) {
     std::cout << "\n" << function(coefficients, inputs[i]) << "  ";
   }
